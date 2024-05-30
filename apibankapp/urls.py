860dea4ab23e8b9ff11abd813522b8b51741b002
pkrecz <pkrecz@poczta.onet.pl>
2024-05-30 15:55:14 +0200

    URLs correction

--- apibankapp/urls.py
+++ apibankapp/urls.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from rest_framework import routers
 from .views import (CustomerViewSet, AccountViewSet, AccountTypeViewSet, ParameterViewSet)
 
@@ -14,4 +16,4 @@ router.register(r'parameter', ParameterViewSet)
 urlpatterns = [
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace = 'rest_framework')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
